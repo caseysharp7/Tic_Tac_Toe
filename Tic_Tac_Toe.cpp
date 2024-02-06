cbdab6964a6ffee2da9a772147ab09b223e1a4a9
caseysharp7 <84864049+caseysharp7@users.noreply.github.com>
2024-02-06 14:35:30 -0800

    Add files via upload

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -47,12 +47,13 @@ void playerTurn(){
     numTurns++;
 }
 std::string victor(){
-    for(int i = 0; i < 3; i++){
-        if(((board[i][0] == board[i][1] == board[i][2]) && board[i][0] == 'X') ||
-           (board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] == 'X')){
-            return "X is victorious\n";
-        } else if((board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] == 'O') || (board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] == 'O')){
-            return "O is victorius\n";
+     for(int i = 0; i < 3; i++){
+        if((board[i][0] == board[i][1] and board[i][0] == board[i][2] and board[i][0] == 'X') or
+           (board[0][i] == board[1][i] and board[0][i] == board[2][i] and board[i][0] == 'X')){
+            return "X is victorius";
+        } else if((board[i][0] == board[i][1] and board[i][0] == board[i][2] and board[i][0] == 'O') or
+           (board[0][i] == board[1][i] and board[0][i] == board[2][i] and board[i][0] == 'O')){
+            return "O is victorius";
         }
     }
     if((board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] == 'X') || (board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[2][0] == 'X')){
